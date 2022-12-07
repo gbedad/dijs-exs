7bfbeabdbe1f7c0c7f1898156dcec3faa6eaa93b
gbedad <gerald.berrebi@gmail.com>
2022-12-07 10:35:49 +0100

    Commit ex xp

---
+++ week9/day4/ex_xp/exercise_xp.js
@@ -0,0 +1,34 @@
+// Exercise XP
+
+let xhr = new XMLHttpRequest();
+
+const getGif = () => {
+  xhr.open(
+    'GET',
+    'https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My'
+  );
+  xhr.responseType = 'json';
+  xhr.send();
+};
+
+// Exercise 2
+
+const getGifsAboutSun = () => {
+  xhr.open(
+    'GET',
+    'https://api.giphy.com/v1/gifs/search/tags?q=sun&limit=10&offset=2&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My'
+  );
+  xhr.responseType = 'json';
+  xhr.send();
+};
+
+xhr.onload = function () {
+  if (xhr.status === 200) {
+    let res = xhr.response.data;
+    console.log(res);
+  }
+};
+
+//getGif(); // Exercise 1
+
+getGifsAboutSun(); // Exercise 2
