109af724f77fff02729f2cbd10abc52aa6624035
gbedad <gerald.berrebi@gmail.com>
2022-12-13 13:31:35 +0100

    Complete part 2 of daily challenge

--- week10/day2/dailyChallenge/script.js
+++ week10/day2/dailyChallenge/script.js
@@ -40,3 +40,105 @@ makeAllCaps(['apple', 'pear', 'banana', 'melon', 'kiwi'])
   .then((arr) => sortWords(arr))
   .then((result) => console.log(result)) //["APPLE","BANANA", "KIWI", "MELON", "PEAR"]
   .catch((error) => console.log(error));
+
+// Daily Challenge 2
+
+const morse = `{
+    "0": "-----",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": "--.",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--..",
+    ".": ".-.-.-",
+    ",": "--..--",
+    "?": "..--..",
+    "!": "-.-.--",
+    "-": "-....-",
+    "/": "-..-.",
+    "@": ".--.-.",
+    "(": "-.--.",
+    ")": "-.--.-"
+  }`;
+
+function toJs() {
+  return new Promise((resolve, reject) => {
+    if (morse) {
+      resolve(JSON.parse(morse));
+    } else {
+      reject('File not in txt');
+    }
+  });
+}
+
+let userWord;
+
+function toMorse(morseJs) {
+  let morseArr = [];
+  let errArr = [];
+  userWord = prompt('Enter a word');
+  let wordArr = userWord.toLocaleLowerCase().split('');
+  wordArr.forEach((item) => {
+    if (morseJs.hasOwnProperty(item)) {
+      morseArr.push(morseJs[item]);
+    } else {
+      errArr.push(item);
+    }
+  });
+  return new Promise((resolve, reject) => {
+    if (morseArr.length === wordArr.length) {
+      resolve(morseArr);
+    } else {
+      reject(
+        `One or more characters (${errArr.join('/')}) is not in morse alphabet`
+      );
+    }
+  });
+}
+
+function joinWords(morseTranslation) {
+  let div = document.getElementById('root');
+  div.textContent = `"${userWord}" gives you :`;
+
+  morseTranslation.map((item) => {
+    let pg = document.createElement('p');
+    div.appendChild(pg);
+
+    pg.innerText = item;
+  });
+}
+
+toJs()
+  .then((res) => toMorse(res))
+  .then((result) => joinWords(result))
+  .catch((err) => console.log(err));
