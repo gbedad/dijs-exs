b77deaedf21d85923790acd2d7d1dfd5aaf7872c
gbedad <gerald.berrebi@gmail.com>
2022-12-15 16:22:22 +0100

    commit first part without loader

---
+++ week10/day5/miniprojects/starwars/script.js
@@ -0,0 +1,32 @@
+const getStarWarsCharacter = () => {
+  const getRandomCharacter = async () => {
+    let randomNumber = Math.floor(Math.random() * (82 - 0) + 0);
+    const response1 = await fetch(
+      `https://www.swapi.tech/api/people/${randomNumber}`
+    );
+
+    try {
+      let character = await response1.json();
+      const selectedCharacter = await character.result.properties;
+      console.log('prop', selectedCharacter);
+      const response2 = await fetch(selectedCharacter.homeworld);
+      let home = await response2.json();
+      const completeCharacter = await Promise.all([selectedCharacter, home]);
+      console.log(completeCharacter);
+      return { details: completeCharacter[0], home: completeCharacter[1] };
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  getRandomCharacter().then((result) => {
+    const content = `
+            <h2>${result.details.name}</h2>
+            <p>Height: ${result.details.height}</p>
+            <p>Gender: ${result.details.gender}</p>
+            <p>Birth Year: ${result.details.birth_year}</p>
+            <p>Home World: ${result.home.result.properties.name}</p>
+  `;
+    document.getElementById('people').innerHTML = content;
+  });
+};
