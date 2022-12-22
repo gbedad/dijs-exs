4b7aefac085f01de0659fe7c389457044dc8ee20
gbedad <gerald.berrebi@gmail.com>
2022-12-22 09:09:30 +0200

    dailychallenge

---
+++ week11/day4/dailyChallenge/sqlPuzzle.sql
@@ -0,0 +1,25 @@
+-- This script will create FirstTab table with 2 fields id and name and insert into this table 4 items. The last one does not have an id. Then it will display all the items.
+-- The same will be done with the second table. In secondTab the second item will be null.
+/* CREATE TABLE FirstTab (
+     id integer, 
+     name VARCHAR(10)
+)
+
+INSERT INTO FirstTab VALUES
+(5,'Pawan'),
+(6,'Sharlee'),
+(7,'Krish'),
+(NULL,'Avtaar')
+
+SELECT * FROM FirstTab
+
+CREATE TABLE SecondTab (
+    id integer 
+)
+
+INSERT INTO SecondTab VALUES
+(5),
+(NULL)
+
+
+SELECT * FROM SecondTab */
