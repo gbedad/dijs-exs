62dfc97f8d0a47b5dc380e5b69a4340cb6de1e8a
gbedad <gerald.berrebi@gmail.com>
2023-01-02 07:23:50 +0100

    Commit exercise xp

---
+++ week13/day2/exercise_xp/npm-node/main.js
@@ -0,0 +1,26 @@
+const a = 5;
+const b = 10;
+let x;
+const c = 6;
+x = a + b;
+
+// Exercise 1
+/* const sayHello1 = (x, y) => {
+  console.log(`Hello you are ${x + y}`);
+};
+
+sayHello1(a, b); */
+
+// Exercise 2
+/* const sayHello2 = (x, y) => {
+  console.log(`Hello you are ${x + y}, how are you?`);
+};
+
+sayHello2(a, b); */
+
+// Exercise 3
+const sayHello3 = (x, y) => {
+  console.log(`Hello you are now ${x + y}, how are you?`);
+};
+
+sayHello3(x, c);
